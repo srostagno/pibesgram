3144bde15d8832034a47cee7081d704c4d621158
“srostagno” <srostagno@gmail.comssh-keygen -t rsa -C srostagno@gmail.comcat>
2016-07-03 12:46:50 -0300

    Cambios a server.js para heroku

--- server.js
+++ server.js
@@ -7,14 +7,21 @@ app.set('view engine', 'pug');
 //nuevo
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    response.render('index', { title: 'PibesGram' });
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 //
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  res.render('index', { title: 'PibesGram' });
-})
+//app.get('/', function(req, res) {
+
+//  res.render('index', { title: 'PibesGram' });
+//})
 
 app.get('/signup', function(req, res) {
   res.render('index', { title: 'PibesGram - SignUp' });
@@ -24,8 +31,8 @@ app.get('/signin', function(req, res) {
   res.render('index', { title: 'PibesGram - SignIn' });
 })
 
-app.listen(3000, function (err) {
-  if (err) return console.log('Hubo un error'), process.exit(1);
+//app.listen(3000, function (err) {
+//  if (err) return console.log('Hubo un error'), process.exit(1);
 
-  console.log('Platzigram escuchando en el puerto 3000');
-})
+//  console.log('Platzigram escuchando en el puerto 3000');
+//})
